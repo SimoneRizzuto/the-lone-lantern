3d170eda62b6e77f5a8975336536675c958216a1
Simorico <simorico.dev@gmail.com>
2023-01-11 23:16:31 +1000

    Started working on new input system. WIP -> trying to get animations working

--- Assets/CSharpScripts/Player/Input/P_Movement.cs
+++ Assets/CSharpScripts/Player/Input/P_Movement.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using CSharpScripts;
 using CSharpScripts.Constants;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class P_Movement : MonoBehaviour
 {
@@ -12,8 +15,12 @@ public class P_Movement : MonoBehaviour
     private readonly float moveSpeed = 4;
     private float xMovement, yMovement;
 
+    private PlayerInput playerInput;
+    
     void Start()
     {
+        playerInput.SwitchCurrentActionMap("Luce Map");
+        
         Physics2D.gravity = Vector2.zero;
         player_rb2d = GetComponent<Rigidbody2D>();
     }
@@ -25,11 +32,11 @@ public class P_Movement : MonoBehaviour
         // => var isCollidingWithCutsceneTrigger = GameObject.FindObjectOfType<OBJ_IsColliding_CutsceneTrigger>().IsCollidingWithPlayer();
         // Change game state to cutscene
         //var isCollidingWithInteractableObject = GameObject.FindObjectOfType<IB_IsColliding_Interactable>().IsCollidingWithInteractable();
+        
+        //xMovement = 0;
+        //yMovement = 0;
 
-        xMovement = 0;
-        yMovement = 0;
-
-        player_rb2d.velocity = new Vector2(yMovement, xMovement);
+        /*player_rb2d.velocity = new Vector2(yMovement, xMovement);
         player_animator.SetFloat("Speed", 0);
 
         player_animator.SetBool("Up", false);
@@ -37,9 +44,11 @@ public class P_Movement : MonoBehaviour
         player_animator.SetBool("Down", false);
         player_animator.SetBool("Right", false);
         
-        if (currentGameState is GameState.Interaction or GameState.Menu or GameState.Cutscene) return;
+        if (currentGameState is GameState.Interaction or GameState.Menu or GameState.Cutscene) return;*/
         
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        
+        
+        /*if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
         {
             ResetMovement();
             return;
@@ -64,20 +73,23 @@ public class P_Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             yMovement = -moveSpeed;
-            player_rb2d.velocity += new Vector2(0, -moveSpeed);
+            player_rb2d.velocity += new Vector2(0, yMovement);
             player_animator.SetFloat("Speed", moveSpeed);
             player_animator.SetBool("Down", true);
             player_animator.SetInteger("LastDirection", 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            xMovement = moveSpeed;
+            xMovement = moveSpeed;  
             player_rb2d.velocity += new Vector2(xMovement, 0);
             player_animator.SetFloat("Speed", moveSpeed);
             player_animator.SetBool("Right", true);
             player_animator.SetInteger("LastDirection", 3);
         }
 
+        
+        
+        
         // If multiple directions are held, stop in the spot.
         if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
         {
@@ -128,7 +140,7 @@ public class P_Movement : MonoBehaviour
             }
 
             player_rb2d.velocity = new Vector2(xMovement, yMovement);
-        }
+        }*/
 
         // Keeps movement to maximum speed.
         LimitVelocityToMaxSpeed();
@@ -152,6 +164,30 @@ public class P_Movement : MonoBehaviour
         player_animator.SetBool("Left", false);
         player_animator.SetBool("Down", false);
         player_animator.SetBool("Right", false);
+    }
+    void OnMove(InputValue value)
+    {
+        xMovement = value.Get<Vector2>().x * max_speed; 
+        yMovement = value.Get<Vector2>().y * max_speed;
+        
+        player_rb2d.velocity = new Vector2(xMovement, yMovement);
+        player_animator.SetFloat("Speed", 0);
+
+        player_animator.SetBool("Up", false);
+        player_animator.SetBool("Left", false);
+        player_animator.SetBool("Down", false);
+        player_animator.SetBool("Right", false);
+        
+        player_animator.SetBool("Up", true);
+        
+        if (yMovement > 0) player_animator.SetBool("Up", true);
+        if (xMovement < 0) player_animator.SetBool("Left", true);
+        if (yMovement < 0) player_animator.SetBool("Down", true);
+        if (xMovement > 0) player_animator.SetBool("Right", true);
+        
+        Debug.Log($"X: {value.Get<Vector2>().x}");
+        Debug.Log($"Y: {value.Get<Vector2>().y}");
+        //Debug.Log($"Action: {action}");
     }
 }
 
